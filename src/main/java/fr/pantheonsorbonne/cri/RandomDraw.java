d4e17fe509b5bf513cabfc42c75a39a51e39a41d
Ahmed Amine SAHNOUN <ahmedaminesahnoun@gmail.com>
2023-10-26 11:35:39 +0200

    Ajout de la classe de tirage au sort

---
+++ src/main/java/fr/pantheonsorbonne/cri/RandomDraw.java
@@ -0,0 +1,19 @@
+package fr.pantheonsorbonne.cri;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class RandomDraw {
+    
+    static  Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        int joueur1 = random.nextInt(2) + 1;
+        int joueur2 = 3 - joueur1; 
+        System.out.println("Le joueur " + joueur1 + " commence. donne des coordonnées x et y ainsi que le nombre que tu veux mettre sur la case:");
+        int x = sc.nextInt();
+        int y= sc.nextInt();
+        int nbplace= sc.nextInt();
+    }       
+}
